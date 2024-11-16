9a1f7a3601c8ad1d9cb1a1ad93aba2620c7d484d
Arystan <61510399+whiteLion95@users.noreply.github.com>
2024-11-16 12:26:54 +0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+const string ANGULAR_CORS_POLICY = "AngularPolicy";
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<DataContext>(options =>
@@ -9,9 +11,20 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(connString);
 });
 builder.Services.AddControllers();
+builder.Services.AddCors(options => {
+    options.AddPolicy(ANGULAR_CORS_POLICY, policy => {
+        policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("https://localhost:4200");
+    });
+});
 
 var app = builder.Build();
 
+app.UseRouting();
+app.UseCors(ANGULAR_CORS_POLICY);
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title = 'Dating app';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('https://localhost:5001/api/users').subscribe(response => {
+      this.users = response;
+    });
+  }
+}
